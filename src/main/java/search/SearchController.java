934ac3b77d60a0033863b635bf9382166e0a6bec
Eyo Ndem <eyondem@eyos-mbp.mynet>
2019-01-27 15:47:44 +0000

    Setting up git ignore

---
+++ src/main/java/search/SearchController.java
@@ -0,0 +1,14 @@
+package search;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class SearchController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
